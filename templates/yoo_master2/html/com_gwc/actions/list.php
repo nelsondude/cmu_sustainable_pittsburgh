a2aa1d4467d32cb3c069c8af24aa7e6fd652fcdb
nelsondude <alexn1336@gmail.com>
2019-03-19 13:05:35 -0400

    Show clickable links in bootstrap accordion view

--- templates/yoo_master2/html/com_gwc/actions/list.php
+++ templates/yoo_master2/html/com_gwc/actions/list.php
@@ -6,6 +6,8 @@ defined('_JEXEC') or die;
 
 $cat = '';
 
+$stripped_category = '';
+
 $ongoing = gwcHelper::ongoing();
 
 usort($this->items,function($a,$b){
@@ -20,9 +22,75 @@ usort($this->items,function($a,$b){
 	else return strcmp($keya,$keyb);
 });
 
+//Group by the item category
+$grouped = array();
+foreach ($this->items as $i => $item) {
+    $grouped[$item->category][] = $item;
+}
+//var_dump($grouped);
+
 if($_GET["debugActionsObj"]) echo "<pre>".print_r($this->items,1)."</pre>";
 
 ?>
+
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<div id="accordion_search_bar_container">
+    <input type="search"
+           id="accordion_search_bar"
+           placeholder="Search"/>
+</div>
+
+<div class="panel-group"
+     id="accordion"
+     role="tablist"
+     aria-multiselectable="true">
+    <?php foreach ($grouped as $category => $items) :?>
+    <?php $stripped_category = str_replace(' ', '', $category) ?>
+        <div class="panel panel-success"
+             id="collapse<?php echo $stripped_category;?>_container">
+            <div class="panel-heading"
+                 role="tab"
+                 id="headingOne">
+                <h4 class="panel-title">
+                    <a role="button"
+                       data-toggle="collapse"
+                       data-target='#collapse<?php echo $stripped_category?>'
+                       href="javascript:void(0);"
+                       aria-expanded="true"
+                       aria-controls="collapse<?php echo $stripped_category;?>">
+                        <?php echo $category ?>
+                    </a>
+                </h4>
+            </div>
+            <div id="collapse<?php echo $stripped_category;?>"
+                 class="panel-collapse collapse in"
+                 role="tabpanel"
+                 aria-labelledby="headingOne">
+                <div class="panel-body">
+                    <ul>
+                        <?php foreach ($items as $index => $item) : ?>
+                            <li>
+                                <?php if ($ongoing): ?>
+                                    <a class=""
+                                       href="index.php?option=com_gwc&view=actions&layout=default&id=<?php echo $item->id; ?>">
+                                        <?php echo $item->name; ?>
+                                    </a>
+                                <?php else : ?>
+                                    <?php echo $item->name; ?>
+                                <?php endif; ?>
+                            </li>
+                        <?php endforeach;?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    <?php endforeach;?>
+</div>
 
 <ul class="actionlist">
 
